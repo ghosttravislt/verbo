75d5db231d94d6cbc65c7a9f4b9f62219a7f3d65
ghosttravislt <enochbaafi56@gmail.com>
2024-08-22 14:37:30 +0100

    Added copy button to the app

--- speech.js
+++ speech.js
@@ -1,7 +1,7 @@
 let clicked = document.querySelector("#start");
 let speech = document.querySelector(".speech");
 let ring = document.querySelector(".ring");
-console.log(clicked);
+let copy = document.querySelector(".copy");
 
 let speechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -25,7 +25,7 @@ clicked.addEventListener("click", () => {
     ring.style.color = "black";
     const synth = window.speechSynthesis;
     let utterThis = new SpeechSynthesisUtterance();
-    utterThis.rate = 2;
+    utterThis.rate = 1.5;
     let talk = (utterThis.text = finalTranscript);
     console.log(talk);
     synth.speak(utterThis);
@@ -33,7 +33,12 @@ clicked.addEventListener("click", () => {
     speechApi.stop();
     setInterval(() => {
       speech.innerText = "";
-      console.log("transcript clean");
+      alert("transcript clean");
     }, 1000 * 60);
   };
+});
+
+copy.addEventListener("click", () => {
+  navigator.clipboard.writeText(finalTranscript);
+  alert("text copied ");
 });
